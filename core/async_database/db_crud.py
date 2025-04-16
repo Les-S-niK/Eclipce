b9c7a6263371dfef026a1f20e9d6da767e0923c4
SkilBox <102877548+TheSkilBox@users.noreply.github.com>
2025-04-16 23:51:59 +0200

    AsymmetricKeys database / Hooks updated

--- core/async_database/db_crud.py
+++ core/async_database/db_crud.py
@@ -4,7 +4,7 @@ from sqlalchemy.exc import OperationalError
 
 ## Project modules:
 from core.async_database.db_engine import session_factory, engine
-from core.async_database.db_models import Users, Base
+from core.async_database.db_models import Users, Base, AsymmetricKeys
 
 ## Create tables
 async def create_tables():
@@ -22,7 +22,6 @@ class UserHook:
         """Append element in table
 
         Args: 
-            table (str): name of table (users/)
             **kwargs: data (login/hashed_password)
         """
         try:
@@ -84,6 +83,87 @@ class UserHook:
         Args:
             object (): object of element
             **flag: selector (id/login/hashed_password)
+        """
+        try:
+            async with session_factory() as session:
+                for obj in object:
+                    session.add(obj)
+                    for key, value in flag.items():
+                        setattr(obj, key, value)
+                
+                await session.commit()
+                return True
+                    
+        except Exception as error:
+            await session.rollback()   
+            return error
+
+class AsymmetricKeysHook:
+    async def append(self, **kwargs):
+        """Append element in table
+
+        Args: 
+            **kwargs: data (private_key/public_key)
+        """
+        try:
+            async with session_factory() as session:
+                key: AsymmetricKeys = AsymmetricKeys(public_key=kwargs["public_key"], private_key=kwargs["private_key"])
+                session.add(key)
+                await session.commit()
+                return True
+
+        except OperationalError:
+            await session.rollback()
+            return Users(**kwargs)
+        except Exception:
+            await session.rollback()
+            return False
+    
+    async def remove(self, all: bool = False, **flag):
+        """Remove element in table
+
+        Args:
+            **flag: selector (id/private_key/public_key)
+        """
+        try:
+            async with session_factory() as session:
+                query = select(AsymmetricKeys).filter_by(**flag)
+                result = await session.execute(query)
+                keys: AsymmetricKeys = result.scalars().first() if all is False else result.scalars().all()
+                for key in keys: 
+                    await session.delete(key)
+                await session.commit()
+                return True
+
+        except Exception:
+            await session.rollback()
+            return False
+        
+    async def get(self, one_object: bool = False, **flag):
+        """Get element in table
+
+        Args:
+            obj (bool): set true if you wanna return asymmetric_key object
+            **flag: selector (id/private_key/public_key)
+        """
+        try:
+            async with session_factory() as session:
+                query = select(AsymmetricKeys).filter_by(**flag)
+                result = await session.execute(query)
+                keys: list[AsymmetricKeys] = result.scalars().all()
+                if one_object and keys != []:
+                    return keys[0]
+                return keys
+
+        except Exception:
+            return False
+        
+    async def replace(self, object, all: bool = True, **flag):
+        """Replace info in object
+
+        Args:
+            object (): object of element
+            **flag: selector (id/private_key/public_key)
         """
         try:
             async with session_factory() as session:
--- core/async_database/db_models.py
+++ core/async_database/db_models.py
@@ -1,6 +1,6 @@
 ## Pip modules:
-from sqlalchemy import VARCHAR
-from sqlalchemy.orm import Mapped, mapped_column
+from sqlalchemy import VARCHAR, ForeignKey
+from sqlalchemy.orm import Mapped, mapped_column, relationship
 from sqlalchemy.orm import DeclarativeBase
 
 ## Declarative Base
@@ -16,7 +16,7 @@ class Base(DeclarativeBase):
 class Users(Base):
     __tablename__ = "users"
     login: Mapped[str] = mapped_column(
-        VARCHAR(24),
+        VARCHAR(16),
         nullable=False,
         unique=True
     )
@@ -24,3 +24,28 @@ class Users(Base):
         VARCHAR(128),
         nullable=False
     )
+    chats: Mapped[list["Chats"]] = relationship("ChatsUsers", back_populates="users")
+
+class Chats(Base):
+    __tablename__ = "chats"
+    chat_name: Mapped[str] = mapped_column(
+        VARCHAR(16),
+        nullable=False,
+    )
+    users: Mapped[list["Users"]] = relationship("ChatsUsers", back_populates="chats")
+    messages: Mapped[list["Messages"]] = relationship("Messages", back_populates="chat")
+    
+class Messages(Base):
+    __tablename__ = "messages"
+    chat: Mapped["Chats"] = relationship("ChatsUsers", back_populates="messages")
+    message: Mapped[str] = mapped_column(VARCHAR(1000), nullable=False)
+    
+class ChatsUsers(Base):
+    __tablename__ = "chats_users"
+    chat_id: Mapped[int] = mapped_column(ForeignKey("chats.id"), primary_key=True)
+    user_id: Mapped[int] = mapped_column(ForeignKey("users.id"), primary_key=True)
+    
+class AsymmetricKeys(Base):
+    __tablename__ = "asymmetric_keys"
+    public_key: Mapped[bytes] = mapped_column(VARCHAR(512), nullable=False)
+    private_key: Mapped[bytes | None] = mapped_column(VARCHAR(512), nullable=False)
